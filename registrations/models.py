69c5be06b8043e319abbc8b2365f9f21a38ad412
rosen609 <rosinant@gmail.com>
2019-05-17 06:04:09 +0300

    Add notifications app

--- registrations/models.py
+++ registrations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class 
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    egn = models.CharField(max_length=10)
+    lnc = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    address = models.CharField(max_length=200)
+    mobile_phone = models.CharField(max_length=17,)
